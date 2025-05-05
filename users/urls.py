86dc50bf43277ef25ddb48904eb8b042908a70c1
roycvx <royelias-227-@hotmail.com>
2025-05-05 14:07:45 -0500

    refactor(urls): cambiar la forma de llamado de la funcion logout

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.show_start_page, name='start'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('logout/', LogoutView.as_view(next_page='start'), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
 
     # """URLS del Client"""
     path('client-dashboard/', views.client_dashboard, name='client_dashboard'),
